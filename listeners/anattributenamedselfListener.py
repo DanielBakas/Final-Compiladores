c992454b0de9286831465368fd1fbb3800f9d96e
Daniel Bakas <danielbakas@gmail.com>
2022-04-26 16:30:53 -0500

    Create anattributenamedselfListener.py

--- listeners/anattributenamedselfListener.py
+++ listeners/anattributenamedselfListener.py
@@ -1,7 +1,7 @@
 """
 ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
 # Proyecto Final de Compiladores
-Paquete | `listeners`
+Módulo | `anattributenamedselfListener.py`
 
 Daniel Bakas Amuchástegui   | A01657103
 Santiago Hernández Guerrero | A01027543
@@ -10,7 +10,21 @@ Abril 19, 2022
 ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
 """
 
-from . import *
 
-from .anattributenamedselfListener import *
-from .nomainListener import *
+from util.exceptions import *
+from antlr.coolListener import coolListener
+from antlr.coolParser import coolParser
+
+
+class anattributenamedselfListener(coolListener):
+
+    def __init__(self):
+        self.isNamedSelf = False
+
+    def enterFeature(self, ctx: coolParser.FeatureContext):
+        if ctx.ID().getText() == 'self':
+            self.isNamedSelf = True
+
+    def exitFeature(self, ctx: coolParser.FeatureContext):
+        if (not self.isNamedSelf):
+            raise anattributenamedself()
--- main.py
+++ main.py
@@ -19,6 +19,7 @@ def compile(file):
     parser = coolParser(CommonTokenStream(coolLexer(FileStream(file))))
     tree = parser.program()
     walker = ParseTreeWalker()
+    walker.walk(anattributenamedselfListener(), tree)
     walker.walk(nomainListener(), tree)
 
 

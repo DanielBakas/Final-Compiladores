b98d6c8207fac9711dbb4f0f3fb185f74ea93dec
SantiagoHernandezG <Santiagohernandez2@icloud.com>
2022-04-26 17:43:00 -0500

    Listeners

---
+++ listeners/inheritsselftypeListener.py
@@ -0,0 +1,30 @@
+"""
+––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+# Proyecto Final de Compiladores
+Módulo | `inheritsselftypeListener.py`
+
+Daniel Bakas Amuchástegui   | A01657103
+Santiago Hernández Guerrero | A01027543
+
+Abril 19, 2022
+––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+"""
+
+
+from util.exceptions import *
+from antlr.coolListener import coolListener
+from antlr.coolParser import coolParser
+
+
+class inheritsselftypeListener(coolListener):
+
+    def __init__(self):
+        self.main = False
+
+    def enterKlass(self, ctx: coolParser.KlassContext):
+        if ctx.TYPE(0).getText() == 'Int':
+            self.main = True
+
+    def exitKlass(self, ctx: coolParser.KlassContext):
+        if (not self.main):
+            raise inheritsselftype()

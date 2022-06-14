d198181b3a374e76e24e2868a62d23f9572ef956
SantiagoHernandezG <Santiagohernandez2@icloud.com>
2022-06-14 15:02:15 -0500

    attroverride test

--- listeners/ThreeListener.py
+++ listeners/ThreeListener.py
@@ -1,6 +1,12 @@
+from typing import Type
 from antlr import coolListener, coolParser
-from util.exceptions import attrbadinit
+from util.exceptions import attrbadinit ,attroverride
 
+from antlr.coolListener import coolListener
+from antlr.coolParser import coolParser
+from util.exceptions import redefinedclass, dupformals, signaturechange, overridingmethod4
+
+from util.structure import *
 
 class ThreeListener(coolListener):
     def __init__(self):
@@ -8,4 +14,14 @@ class ThreeListener(coolListener):
 
     def enterKlass(self, ctx: coolParser.KlassContext):
         self.currentClass = ctx.dataType
+
+    def enterAttribute(self, ctx: coolParser.AttributeContext):
+        name = ctx.ID().getText()
+        type = ctx.TYPE().getText()
+
+        try:
+            ctx.currentClass.lookupAttribute(name)
+            raise attroverride
+        except KeyError:
+            ctx.currentClass.addAttribute(name, type)
 
